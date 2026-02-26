3fd7e57dcc2150e613c3ce12f9bb04ea7f17b806
DSM3 <fatec-dsm3@academico.intra>
2026-02-25 22:45:54 -0300

    Criando services e controllers

--- aula-01-criando-a-api/index.js
+++ aula-01-criando-a-api/index.js
@@ -3,31 +3,36 @@ import express from "express";
 import mongoose from'mongoose';
 //importando o Model
 import Game from "./models/Games.js";
+//Importando as Rotas (Routes)
+import gameRoutes from "./routes/gameRoutes.js";
+
 const app = express();
 
 
 //Configurações do Express
 app.use(express.json())//Permite o uso de json na aplicação
+//Ativando a utilização das rotas
+app.use('/', gameRoutes)
 //iniciando a conexão com o banco de dados MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/api-the-games")
-app.get("/", (req, res) => {
-    const games = [
-        {
-            title: "Game 1",
-            year: "2020",
-            platform: "PC",
-            price: 20
-        },
+// app.get("/", (req, res) => {
+//     const games = [
+//         {
+//             title: "Game 1",
+//             year: "2020",
+//             platform: "PC",
+//             price: 20
+//         },
 
-         {
-            title: "Game 2",
-            year: "2024",
-            platform: "Xbox",
-            price: 30
-        }
-    ]
-    res.status(200).json(games)
-});
+//          {
+//             title: "Game 2",
+//             year: "2024",
+//             platform: "Xbox",
+//             price: 30
+//         }
+//     ]
+//     res.status(200).json(games)
+// });
 
 //Rodando a API na porta 4000
 const port = 4000;
